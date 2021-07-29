acf0e109c6facfae9a5002076205124b194b6232
Trevor L Davis <trevor.l.davis@gmail.com>
2021-07-29 12:42:45 -0700

    feat: 'bm_font()', 'is_bm_font()', 'as_bm_font()'
    * `bm_font()` creates a S3 object representing bitmap fonts.
    
      * `as_bm_font()` is a S3 method that coerces objects to `bm_font()` objects
    
        * `as_bm_font.default()`
        * `as_bm_font.list()`
    
      * `is_bm_font()` returns `TRUE` for `bm_font()` objects (or subclasses)
        and `FALSE` for all other objects.
    
    closes #5

---
+++ R/bm_font.R
@@ -0,0 +1,94 @@
+#' Bitmap font object
+#'
+#' `bm_font()` creates a bitmap font object.
+#'
+#' `bm_font()` is a named list.  The names are of the form \dQuote{U+HHHH} or \dQuote{UHHHHH}.
+#' where the `H` are appropriate hexadecimal Unicode codes.
+#' @param x Named list of [bm_glyph()] objects.
+#'          Names must be coercible by [Unicode::as.u_char()].
+#' @examples
+#'  font_file <- system.file("fonts/spleen/spleen-8x16.hex.gz", package = "bittermelon")
+#'  font <- read_hex(font_file)
+#'  is_bm_font(font)
+#'
+#'  # number of characters in font
+#'  length(font)
+#'
+#'  # print out "R"
+#'  R_code_point <- code_point_from_name("LATIN CAPITAL LETTER R") # U+0052
+#'  print(font[[R_code_point]], labels = c(".", "#"))
+#' @return A named list with a \dQuote{bm_font} subclass.
+#' @seealso [is_bm_font()], [as_bm_font()]
+#' @export
+bm_font <- function(x) {
+    if (is_bm_font(x))
+        x
+    else
+        as_bm_font(x)
+}
+
+#' Test if the object is a bitmap font object
+#'
+#' `is_bm_font()` returns `TRUE` for `bm_font` objects (or subclasses)
+#' and `FALSE` for all other objects.
+#' @param x An object
+#' @return `TRUE` or `FALSE`
+#' @examples
+#'  font_file <- system.file("fonts/spleen/spleen-8x16.hex.gz", package = "bittermelon")
+#'  font <- read_hex(font_file)
+#'  is_bm_font(font)
+#' @seealso [bm_font()]
+#' @export
+is_bm_font <- function(x) {
+    inherits(x, "bm_font")
+}
+
+#' Coerce to bitmap font objects
+#'
+#' `as_bm_font()` turns an existing object into a `bm_font()` object.
+#'
+#' @param x An object that can reasonably be coerced to a `bm_font()` object.
+#' @param ... Further arguments passed to or from other methods.
+#' @return A `bm_font()` object.
+#' @examples
+#'   plus_sign <- matrix(0L, nrow = 9L, ncol = 9L)
+#'   plus_sign[5L, 3:7] <- 1L
+#'   plus_sign[3:7, 5L] <- 1L
+#'   plus_sign_glyph <- bm_glyph(plus_sign)
+#'   plus_sign_code_point <- code_point_from_name("PLUS SIGN") # "U+002B"
+#'
+#'   space_glyph <- bm_glyph(matrix(0L, nrow = 9L, ncol = 9L))
+#'   space_code_point <- code_point_from_name("SPACE") # "U+0020"
+#'
+#'   l <- list()
+#'   l[[plus_sign_code_point]] <- plus_sign_glyph
+#'   l[[space_code_point]] <- space_glyph
+#'   font <- as_bm_font(l)
+#'   is_bm_font(font)
+#'
+#' @seealso [bm_font()]
+#' @export
+as_bm_font <- function(x, ...) {
+    UseMethod("as_bm_font")
+}
+
+#' @rdname as_bm_font
+#' @export
+as_bm_font.list <- function(x, ...) {
+    if (!all(sapply(x, is_bm_glyph)))
+        stop("Some elements were not `bm_glyph()` objects")
+    if (is.null(names(x)) || any(names(x) == ""))
+        stop("'x' must be a **named** list (with Unicode code point names)")
+    codepoints <- code_point(names(x))
+    if (any(codepoints == "<NA>"))
+        stop("Some names were not coercible by `Unicode::as_u_char()`")
+    names(x) <- codepoints
+    class(x) <- c("bm_font", class(x))
+    x
+}
+
+#' @rdname as_bm_font
+#' @export
+as_bm_font.default <- function(x, ...) {
+    as_bm_font.list(as.list(x))
+}
---
+++ tests/testthat/test-bm_font.R
@@ -0,0 +1,17 @@
+test_that("bm_font()", {
+    plus_sign <- matrix(0L, nrow = 9L, ncol = 9L)
+    plus_sign[5L, 3:7] <- 1L
+    plus_sign[3:7, 5L] <- 1L
+    plus_sign_glyph <- bm_glyph(plus_sign)
+    plus_sign_code_point <- code_point_from_name("PLUS SIGN") # code point U+002B
+
+    space_glyph <- bm_glyph(matrix(0L, nrow = 9L, ncol = 9L))
+    space_code_point <- code_point_from_name("SPACE") # code point U+0020
+
+    l <- list()
+    l[[plus_sign_code_point]] <- plus_sign_glyph
+    l[[space_code_point]] <- space_glyph
+    font <- as_bm_font(l)
+    expect_true(is_bm_font(font))
+
+})
