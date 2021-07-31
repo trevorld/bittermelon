2bf1d750f4ad513ce90dd98f1cf006cef11a9372
Trevor L Davis <trevor.l.davis@gmail.com>
2021-07-31 16:06:41 -0700

    feat: 'read_monobit()' and 'write_monobit()'
    * Uses 'monobit' to convert to/from the 'yaff' format which
      {bittermelon} can natively handle.
    * Requires that Python is available on the system.
    * `read_monobit()` reads in a bitmap font as a `bm_font()` object using `monobit`.
      Supports multiple bitmap font formats.  Requires Python to be available. (#26)
    * `write_monobit()` writes out a bitmap font of a `bm_font()` object using `monobit`.
      Supports multiple bitmap font formats.  Requires Python to be available. (#27)
    
    closes #26, closes #27

---
+++ R/monobit.R
@@ -0,0 +1,57 @@
+#' Read and write bitmap font files using monobit
+#'
+#' `read_monobit()` reads in bitmap font file as a [bm_font()] object while `write_monobit()`
+#' writes a [bm_font()] object as a bitmap font file.
+#' It uses the file extension to determine the appropriate bitmap font format to use.
+#'
+#' `read_monobit()` and `write_monobit()` require Python v3.6 or greater available on the system.
+#' `read_monobit()` and `write_monobit()` uses `monobit`'s `convert.py` script to convert to/from
+#' the yaff font format which this package can natively read/write from/to.
+#' Note `monobit` is alpha level software which may not always work
+#' (in particular font export may be buggy).
+#'
+#' @param file A character string of a filename.
+#'
+#' @param font A [bm_font()] object.
+#' @examples
+#'  font_file <- system.file("fonts/spleen/spleen-8x16.yaff.gz", package = "bittermelon")
+#'  tempfile <- tempfile(fileext = ".yaff")
+#'  writeLines(readLines(font_file), tempfile)
+#'
+#'  font <- read_monobit(tempfile)
+#'  capital_r <- font[[str2ucp("R")]]
+#'  print(capital_r, labels = c(".", "#"))
+#'
+#'  filename <- tempfile(fileext = ".yaff")
+#'  write_monobit(font, filename)
+#' @seealso [bm_font()] for more information about bitmap font objects.
+#'    [read_hex()], [write_hex()], [read_yaff()], [write_yaff()] for pure R bitmap font readers and writers.
+#'    For more information about `monobit` see <https://github.com/robhagemans/monobit>.
+#' @rdname monobit
+#' @export
+read_monobit <- function(file) {
+    stopifnot(!missing(file))
+    python <- findpython::find_python_cmd(minimum_version = "3.6")
+    convert <- system.file("monobit/convert.py", package = "bittermelon")
+
+    tfile <- tempfile(fileext = ".yaff")
+    on.exit(unlink(tfile))
+
+    system2(python, c(convert, file, tfile))
+    read_yaff(tfile)
+}
+
+#' @rdname monobit
+#' @export
+write_monobit <- function(font, file) {
+    stopifnot(!missing(font), !missing(file))
+    python <- findpython::find_python_cmd(minimum_version = "3.6")
+    convert <- system.file("monobit/convert.py", package = "bittermelon")
+
+    tfile <- tempfile(fileext = ".yaff")
+    on.exit(unlink(tfile))
+
+    write_yaff(font, tfile)
+    system2(python, c(convert, tfile, file))
+    invisible(invisible(NULL))
+}
---
+++ tests/testthat/test-monobit.R
@@ -0,0 +1,21 @@
+test_that("read_monobit() and write_monobit()", {
+    plus_cp <- name2ucp("PLUS SIGN") # code point U+002B
+
+    hex_file <- system.file("fonts/spleen/spleen-8x16.hex.gz", package = "bittermelon")
+    hex <- read_hex(hex_file)
+
+    hex_file_uncompressed <- tempfile(fileext = ".hex")
+    writeLines(readLines(hex_file), hex_file_uncompressed)
+    hex2 <- read_monobit(hex_file_uncompressed)
+    unlink(hex_file_uncompressed)
+
+    expect_true(is_bm_font(hex2))
+    expect_equal(hex[[plus_cp]], hex2[[plus_cp]])
+
+    f <- tempfile(fileext = ".hex")
+    write_monobit(hex2, f)
+    hex3 <- read_monobit(f)
+    unlink(f)
+
+    expect_equal(hex[[plus_cp]], hex3[[plus_cp]])
+})
