59059e02f7dfebf6649baa4d3343f65c2f0aa158
Trevor L Davis <trevor.l.davis@gmail.com>
2021-08-01 08:22:15 -0700

    feat: Allow customizing replacement values in 'bm_clamp()'
    * New `value` argument allows customizing replacement values
      in `bm_clamp()`.
    * Replacing values above '1L' with '0L' instead of default '1L' would
      effectively set any new non-binary pixels to "white" instead of "black".
      Could possibly be used to help undo certain yet-to-be-written effects
      like a "shadow" or a "glow".
    
    closes #29

--- R/bm_clamp.R
+++ R/bm_clamp.R
@@ -1,12 +1,19 @@
 #' Clamp bitmap integer values.
 #'
-#' `bm_clamp()` \dQuote{clamps} bitmap integers to lie between a lower and upper value.
-#' The default lower and upper values coerce the bitmap
+#' `bm_clamp()` \dQuote{clamps} bitmap integers that lie outside an interval.
+#' The default coerces a multiple-integer-valued bitmap
 #' into a binary bitmap (as expected by most bitmap font formats).
 #'
-#' @param bm_object Either a [bm_bitmap()] or [bm_list()] object (including [bm_font()]).
-#' @param lower Integer value.  Any value below `lower` will be clamped to `lower`.
-#' @param upper Integer value.  Any value above `upper` will be clamped to `upper`.
+#' @param bm_object Either a [bm_bitmap()], [bm_list()],  or [bm_font()] object.
+#' @param lower Integer value.  Any value below `lower` will be clamped.
+#' @param upper Integer value.  Any value above `upper` will be clamped.
+#' @param value Integer vector of length one or two of replacement value(s).
+#'              If `value` is length one
+#'              any values above `upper` are replaced by `value`
+#'              while those below `lower` are replaced by `lower`.
+#'              If `value` is length two any values above `upper`
+#'              are replaced by `value[2]` and any values below `lower`
+#'              are replaced by `value[1]`.
 #' @examples
 #'  plus_sign <- matrix(0L, nrow = 9L, ncol = 9L)
 #'  plus_sign[5L, 3:7] <- 2L
@@ -16,21 +23,31 @@
 #'
 #'  plus_sign_clamped <- bm_clamp(plus_sign_glyph)
 #'  print(plus_sign_clamped, labels = c(".", "#", "@"))
-#' @return Either a [bm_bitmap()] or [bm_list()] object (including [bm_font()]).
+#' @return Either a [bm_bitmap()], [bm_list()], or [bm_font()] object.
 #' @export
-bm_clamp <- function(bm_object, lower = 0L, upper = 1L) {
-    modify_bm_bitmaps(bm_object, bm_clamp_bitmap, lower = lower, upper = upper)
+bm_clamp <- function(bm_object, lower = 0L, upper = 1L, value = upper) {
+    stopifnot(length(value) > 0L, length(value) < 3L)
+    if (length(value) == 2L) {
+        value_lower <- value[2L]
+        value_upper <- value[2L]
+    } else {
+        value_lower <- lower
+        value_upper <- value
+    }
+    modify_bm_bitmaps(bm_object, bm_clamp_bitmap, lower = lower, upper = upper,
+                     value_lower = value_lower, value_upper = value_upper)
 }
 
-bm_clamp_bitmap <- function(bitmap, lower = 0L, upper = 1L) { # nolint
+bm_clamp_bitmap <- function(bitmap, lower = 0L, upper = 1L,
+                            value_lower = lower, value_upper = upper) {
     stopifnot(lower <= upper)
     indices <- which(bitmap < lower)
     if (length(indices)) {
-        bitmap[indices] <- lower
+        bitmap[indices] <- value_lower
     }
     indices <- which(bitmap > upper)
     if (length(indices)) {
-        bitmap[indices] <- upper
+        bitmap[indices] <- value_upper
     }
     bitmap
 }
