aa6ee0bf21736f6b6295e7c42a2955e0fcdea92e
Trevor L. Davis <trevor.l.davis@gmail.com>
2024-05-28 11:32:21 -0700

    fix(bm_options()): Update 'monobit_path' and 'col' options

--- R/bittermelon-package.R
+++ R/bittermelon-package.R
@@ -1,19 +1,19 @@
 #' @section Package options:
 #'  The following `bittermelon` option may be set globally via [base::options()]:
 #'   \describe{
-#'     \item{bittermelon.px}{Set new default `px` argument value for
-#'                           [format.bm_bitmap()] and [print.bm_bitmap()].
-#'                           If unset both functions default to [px_auto()].}
-#'     \item{bittermelon.fg}{Set new default `fg` argument value for
-#'                           [format.bm_bitmap()] and [print.bm_bitmap()].}
 #'     \item{bittermelon.bg}{Set new default `bg` argument value for
 #'                           [format.bm_bitmap()] and [print.bm_bitmap()].}
 #'     \item{bittermelon.col}{Set new default `col` argument value for
 #'                           [as.raster.bm_bitmap()] and [plot.bm_bitmap()].}
 #'     \item{bittermelon.compress}{Set new default `compress` argument value for
 #'                           [format.bm_bitmap()] and [print.bm_bitmap()].}
+#'     \item{bittermelon.fg}{Set new default `fg` argument value for
+#'                           [format.bm_bitmap()] and [print.bm_bitmap()].}
 #'     \item{bittermelon.monobit_path}{Set new default `monobit_path` argument value
 #'                           for [read_monobit()] and [write_monobit()].}
+#'     \item{bittermelon.px}{Set new default `px` argument value for
+#'                           [format.bm_bitmap()] and [print.bm_bitmap()].
+#'                           If unset both functions default to [px_auto()].}
 #'   }
 #'  The following `cli` options may also be of interest:
 #'   \describe{
