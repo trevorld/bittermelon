f017f75599fa0c2bfd0bb4ee45038740ba6bdfd5
Trevor L. Davis <trevor.l.davis@gmail.com>
2026-01-14 14:36:23 -0800

    feat: `bm_extract()`
    `bm_extract()` can extract parts of a bitmap.
    For `bm_bitmap()` and `bm_pixmap()` objects it is a wrapper around `[]` with `drop = FALSE` for convenience in pipes.
    Rows are indexed from the **bottom** of the image.
    
    closes #89

---
+++ tests/testthat/test-bm_extract.R
@@ -0,0 +1,40 @@
+test_that("bm_extract()", {
+	skip_if_not_installed("withr")
+	withr::local_options(bm_options(default = TRUE))
+
+	skip_if_not(cli::is_utf8_output())
+	crops <- farming_crops_16x16()
+	corn <- crops$corn$portrait
+
+	corn_top <- bm_extract(corn, 9:16)
+	corn_bot <- bm_extract(corn, 1:8)
+	corn_left <- bm_extract(corn, cols = 1:8)
+	corn_right <- bm_extract(corn, cols = 9:16)
+
+	verify_output(
+		"txt/bm_extract_pixmap.txt",
+		{
+			print(corn, compress = "v")
+			print(corn_top, compress = "v")
+			print(corn_bot, compress = "v")
+			print(corn_left, compress = "v")
+			print(corn_right, compress = "v")
+		},
+		crayon = FALSE,
+		unicode = TRUE
+	)
+
+	corn_l_top <- bm_extract(bm_list(corn), 9:16)
+	expect_equal(corn_top, corn_l_top[[1L]])
+
+	corn_r_top <- bm_extract(as.raster(corn), 9:16)
+	expect_equal(corn_top, as_bm_pixmap(corn_r_top))
+
+	skip_if_not_installed("magick")
+	corn_m_top <- bm_extract(magick::image_read(corn), 9:16)
+	expect_equal(corn_top, as_bm_pixmap(corn_m_top))
+
+	skip_if_not_installed("farver")
+	corn_nr_top <- bm_extract(as.raster(corn, native = TRUE), 9:16)
+	expect_equal(corn_top, as_bm_pixmap(corn_nr_top))
+})
